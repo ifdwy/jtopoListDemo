7bb4c2eaf20ba70aee286711055f11bb1f660f16
ifdwy <1193044859@qq.com>
2020-02-25 17:29:59 +0800

    todo: 分情况隐藏掉圆形节点

---
+++ listDemo/js/jtopoShowData.js
@@ -0,0 +1,211 @@
+
+// var showData ={
+// 	"0":[
+// 		{
+// 			"id": 1,
+// 			"parentId": 0,
+// 			"img": "img/set1.jpg",
+// 			"name": "设备1"
+// 		}
+// 	],
+
+// 	"1":[
+// 		{
+// 			"id": 2,
+// 			"parentId": 1,
+// 			"img": "img/set2.jpg",
+// 			"name": "设备2"
+// 		},
+// 		{
+// 			"id": 3,
+// 			"parentId": 1,
+// 			"img": "img/set3.jpg",
+// 			"name": "设备3"
+// 		}
+// 	],
+
+// 	"3":[
+// 		{
+// 			"id": 4,
+// 			"parentId": 3,
+// 			"img": "img/set4.jpg",
+// 			"name": "设备4"
+// 		},
+// 		{
+// 			"id": 5,
+// 			"parentId": 3,
+// 			"img": "img/set5.jpg",
+// 			"name": "设备5"
+// 		},
+// 		{
+// 			"id": 6,
+// 			"parentId": 3,
+// 			"img": "img/set6.jpg",
+// 			"name": "设备6"
+// 		}
+// 	],
+
+// 	"4":[
+// 		{
+// 			"id": 7,
+// 			"parentId": 4,
+// 			"img": "img/set7.jpg",
+// 			"name": "设备7"
+// 		},
+// 		{
+// 			"id": 8,
+// 			"parentId": 4,
+// 			"img": "img/set8.jpg",
+// 			"name": "设备8"
+// 		}
+// 	]	
+
+// }
+
+
+var dataList=[
+		{
+			"id": 1,
+			"parentId": 0,
+			"img": "img/set1.jpg",
+			"name": "设备1",
+			"isRoot": true,
+			"alarm": false
+		},
+		{
+			"id": 2,
+			"parentId": 1,
+			"img": "img/set2.jpg",
+			"name": "设备2",
+			"isRoot": false,
+			"alarm": true
+		},
+		{
+			"id": 3,
+			"parentId": 1,
+			"img": "img/set3.jpg",
+			"name": "设备3",
+			"isRoot": false,
+			"alarm": false
+		},
+		{
+			"id": 4,
+			"parentId": 3,
+			"img": "img/set4.jpg",
+			"name": "设备4",
+			"isRoot": false,
+			"alarm": false
+		},
+		{
+			"id": 5,
+			"parentId": 3,
+			"img": "img/set5.jpg",
+			"name": "设备5",
+			"isRoot": false,
+			"alarm": true
+		},
+		{
+			"id": 6,
+			"parentId": 3,
+			"img": "img/set6.jpg",
+			"name": "设备6",
+			"isRoot": false,
+			"alarm": false
+		},
+		{
+			"id": 7,
+			"parentId": 5,
+			"img": "img/set7.jpg",
+			"name": "设备7",
+			"isRoot": false,
+			"alarm": true
+		},
+		{
+			"id": 8,
+			"parentId": 4,
+			"img": "img/set8.jpg",
+			"name": "设备8",
+			"isRoot": false,
+			"alarm": true
+		},
+		{
+			"id": 9,
+			"parentId": 8,
+			"img": "img/set9.jpg",
+			"name": "设备9",
+			"isRoot": false,
+			"alarm": false
+		},
+		{
+			"id": 10,
+			"parentId": 8,
+			"img": "img/set10.jpg",
+			"name": "设备10",
+			"isRoot": false,
+			"alarm": true
+		},
+		{
+			"id": 11,
+			"parentId": 9,
+			"img": "img/set11.jpg",
+			"name": "设备11",
+			"isRoot": false,
+			"alarm": true
+		},
+		{
+			"id": 12,
+			"parentId": 10,
+			"img": "img/set12.jpg",
+			"name": "设备12",
+			"isRoot": false,
+			"alarm": false
+		},
+		{
+			"id": 13,
+			"parentId": 10,
+			"img": "img/set13.jpg",
+			"name": "设备13",
+			"isRoot": false,
+			"alarm": true
+		},
+		{
+			"id": 14,
+			"parentId": 11,
+			"img": "img/set14.jpg",
+			"name": "设备14",
+			"isRoot": false,
+			"alarm": true
+		},
+		{
+			"id": 15,
+			"parentId": 11,
+			"img": "img/set15.jpg",
+			"name": "设备15",
+			"isRoot": false,
+			"alarm": false
+		},
+		{
+			"id": 16,
+			"parentId": 11,
+			"img": "img/set16.jpg",
+			"name": "设备16",
+			"isRoot": false,
+			"alarm": true
+		},
+		{
+			"id": 17,
+			"parentId": 14,
+			"img": "img/set17.jpg",
+			"name": "设备17",
+			"isRoot": false,
+			"alarm": false
+		},
+		{
+			"id": 18,
+			"parentId": 14,
+			"img": "img/set18.jpg",
+			"name": "设备18",
+			"isRoot": false,
+			"alarm": true
+		}				
+]
